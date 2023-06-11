d00f5c41b692d975af77951a1e2fca950425765b
ravidro1 <ravidrrt@gmail.com>
2023-06-11 12:54:42 +0300

    fix: board-hover

--- src/Pages/OneVsOne.jsx
+++ src/Pages/OneVsOne.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
 import { Context } from "../App";
 import "./gamePage.css";
 
